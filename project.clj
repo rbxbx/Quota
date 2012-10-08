cf6723b4bf50b3300af26918579aa719981a1d1e
Robert Pitts <rbxbxdev@gmail.com>
2012-10-08 14:17:14 -0700

    Init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject quota2 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.1"]]
+  :plugins [[lein-ring "0.7.3"]]
+  :ring {:handler quota2.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
